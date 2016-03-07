239438fa03bb169eed0da351da4e2c302c450585
VADIM KASSIN <vgzulus@gmail.com>
2016-03-07 20:51:39 +0600

    first commit

---
+++ iMTS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iMTS
+//
+//  Created by VADIM KASSIN on 3/7/16.
+//  Copyright © 2016 VADIM KASSIN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
